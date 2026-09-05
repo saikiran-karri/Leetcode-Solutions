1ad2c343437f4c8c2ca42de0eb2ab11d7da00713
saikiran karri <saikiran.karri06@gmail.com>
2026-09-05 11:10:15 +0530

    Time: 5 ms (16.72%), Space: 263.5 MB (13.43%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+
+        int count = 0;
+
+        for (int start = 0; count < n; start++) {
+            int current = start;
+            int prev = nums[start];
+
+            do {
+                int next = (current + k) % n;
+                swap(prev, nums[next]);
+
+                current = next;
+                count++;
+
+            } while (current != start);
+        }
+    }
+};
